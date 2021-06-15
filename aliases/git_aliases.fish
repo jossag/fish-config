dbef3dee83a7089c4beadb59cccf06b15fc95012
Jonas Sandberg <jonas.sandberg@codemill.se>
2021-06-15 23:04:39 +0200

    Update git aliases

--- aliases/git_aliases.fish
+++ aliases/git_aliases.fish
@@ -1,3 +1,5 @@
 set -Ux GIT_MAIN_BRANCH main
 
 abbr -a -g gs git status
+alias gl="git log --pretty=format:'%C(yellow)%h %Cred%ad %Cblue%an%Cgreen%d %Creset%s' --date=short"
+abbr -a -g gpl git pull
